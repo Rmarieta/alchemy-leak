65e59786a358aa4d86a4ff86a4dd8b50bc5ba942
Rmarieta <raphael.marietan@hotmail.com>
2023-12-15 14:40:10 +0100

    Small typo

--- alchemy/main.py
+++ alchemy/main.py
@@ -3,7 +3,7 @@ from app.models import Base, Event
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-engine = create_engine(db_uri, echo=True)
+engine = create_engine(db_uri)
 Base.metadata.create_all(engine)
 
 nb_entries = 50000
@@ -11,9 +11,6 @@ notebook_id = 'postman_python'
 content = "wngfiwrhguirehgiuerhgiuthiuthbirubntruituihthbrtwngfiwrhguirehgiuerhgiuthiuthbirubntruituihthbrtwngfiwrhguirehgiuerhgiuthiuthbirubntruituihthbrtwngfiwrhguirehgiuerhgiuthiuthbirubntruituihthbrtwng"
 
 if __name__ == "__main__":
-
-    engine = create_engine(db_uri)
-    Base.metadata.bind = engine 
 
     Session = sessionmaker(bind=engine)
     session = Session()
